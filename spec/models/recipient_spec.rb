930d2a0aff9353c8009982bf55f91de48ff53720
SzeChan <chansd5@gmail.com>
2015-07-02 21:04:47 -0400

    Refactored recipient_spec Rspec test

--- spec/models/recipient_spec.rb
+++ spec/models/recipient_spec.rb
@@ -2,34 +2,34 @@ require 'rails_helper'
 
 describe Recipient do
   it "has a valid factory" do
-  	expect(FactoryGirl.build(:recipient)).to be_valid
+  	expect(build(:recipient)).to be_valid
   end
 
   it "is valid with a name, phone, and belongs to a user" do
-  	recipient = FactoryGirl.build(:recipient)
+  	recipient = build(:recipient)
   	expect(recipient).to be_valid
   end
 
   it "is invalid without a name" do
-  	recipient = FactoryGirl.build(:recipient, name: '')
+  	recipient = build(:recipient, name: '')
   	recipient.valid?
   	expect(recipient.errors[:name]).to include("can't be blank")
   end
 
   it "is invalid with a name shorter than three characters" do
-  	recipient = FactoryGirl.build(:recipient, name: 'Bo')
+  	recipient = build(:recipient, name: 'Bo')
   	recipient.valid?	
   	expect(recipient.errors[:name]).to include("is too short (minimum is 3 characters)")
   end
 
   it "is invalid without a phone" do
-  	recipient = FactoryGirl.build(:recipient, phone: '')
+  	recipient = build(:recipient, phone: '')
   	recipient.valid?
   	expect(recipient.errors[:phone]).to include("can't be blank")
   end
 
   it "is invalid if it doesn't belong to a user" do
-    recipient = FactoryGirl.build(:recipient, user_id: '')
+    recipient = build(:recipient, user_id: '')
     recipient.valid?
     expect(recipient.errors[:user_id]).to include("can't be blank")
   end
