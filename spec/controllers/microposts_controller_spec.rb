1767153db6f2e42fef31b366ac71edc1982501b5
SzeChan <chansd5@gmail.com>
2015-07-07 18:03:28 -0400

    Wrote initial MicropostController test for core functionality, STATUS: Failing

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe MicropostsController, :type => :controller do
-  # include SmsSpec::Helpers
-  # include SmsSpec::Matchers
+  
+  # Typical user login 
+  describe 'logged in user'
+    before :each do
+  	  user = create(:user)
+  	  session[:user_id] = user.id
+    end
 
   # Remember to update scheduled time if testing
   describe 'will schedule and send sms' do
-  	before(:each) do
+  	before :each do
   	  test_time = Time.utc(2015, 7, 6, 22, 0, 0)
   	  Timecop.freeze(test_time)
   	end
@@ -14,7 +19,8 @@ RSpec.describe MicropostsController, :type => :controller do
   	after(:each) { Timecop.return }
 
   	it "only at time specified by its scheduled_time column" do
-  	  valid_logged_in_user
+  	  #valid_logged_in_user
+  	  expect is_logged_in?.to eq(true)
   	  valid_SMS_form_input
   	  expect(Delayed::Job.count).to eq(1)
   	  scheduled_time_of_test_sms = Time.utc(2015, 7, 6, 22, 0, 0)
