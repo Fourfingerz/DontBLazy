1a58bea0be99989d353a41b98ea28ff00954150a
SzeChan <chansd5@gmail.com>
2015-07-06 20:46:47 -0400

    Wrote test for core scheduled SMS core functionality with timecop with Rspec within the MicropostsController spec

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -3,10 +3,27 @@ require 'rails_helper'
 RSpec.describe MicropostsController, :type => :controller do
   include SmsSpec::Helpers
 
-  describe "POST 'schedule'" do
+  # Remember to update scheduled time if testing
+  describe 'will schedule and send sms' do
+  	before(:each) do
+  	  test_time = Time.utc(2015, 7, 6, 22, 0, 0)
+  	  Timecop.freeze(test_time)
+  	end
 
-  	it "schedules time for micropost (task) and sends SMS to phone at that time" do
-  	  
+  	after(:each) { Timecop.return }
+
+  	it "will only at time specified by its scheduled_time column" do
+  	  valid_sign_in
+  	  valid_SMS_form_input
+  	  expect(Delayed::Job.count).to eq(1)
+  	  scheduled_time_of_test_sms = Time.utc(2015, 7, 6, 22, 0, 0)
+  	  expect(Micropost.first.scheduled_time).to eq(scheduled_time_of_test_sms)
+  	  Timecop.travel(Time.now + 5.days)
+  	  successes, failures = Delayed::Worker.new.work_off
+  	  expect(successes).to eq(1)
+  	  expect(failures).to eq(0)
+  	  expect(Delayed::Job.count).to eq(0)
+  	  expect(Micropost.first.sms_sent).to eq(true)
   	end
 
   end
