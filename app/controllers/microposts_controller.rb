0cd423fae33994872dc28ab5870aecdbdbaf8739
SzeChan <chansd5@gmail.com>
2015-07-10 15:23:07 -0400

    Added schedule_time to permitted parameters on MicropostController to accept form input properly

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -22,7 +22,7 @@ class MicropostsController < ApplicationController
   private
     
     def micropost_params
-      params.require(:micropost).permit(:content, :picture)
+      params.require(:micropost).permit(:content, :picture, :schedule_time)
     end
     
     def correct_user
