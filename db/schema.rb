913d785cea93cd63d1193507aa762abfe5271094
SzeChan <chansd5@gmail.com>
2015-07-20 21:03:23 -0400

    Migrated micropost_id_due_now:integer to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720201614) do
+ActiveRecord::Schema.define(version: 20150721010225) do
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
@@ -74,13 +74,13 @@ ActiveRecord::Schema.define(version: 20150720201614) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",             default: false
+    t.boolean  "admin",                default: false
     t.string   "activation_digest"
-    t.boolean  "activated",         default: false
+    t.boolean  "activated",            default: false
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 20150720201614) do
     t.string   "phone_pin"
     t.boolean  "phone_verified"
     t.text     "current_tasks_map"
+    t.integer  "micropost_id_due_now"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
