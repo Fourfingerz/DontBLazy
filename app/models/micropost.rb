082562483d07f780455c976b93589a8ab3c04a7b
SzeChan <chansd5@gmail.com>
2015-07-27 20:04:55 -0400

    Rescheduling instead of making multiple delayed jobs

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -11,7 +11,7 @@ class Micropost < ActiveRecord::Base
   has_many :recipients, through: :micropost_recipients
   validates :days_to_complete, presence: true
   after_create :set_initial_state
-  after_create :schedule_check_in_deadlines
+  after_create :schedule_check_in_deadline
 
   # DBL Logic
 
@@ -86,23 +86,28 @@ class Micropost < ActiveRecord::Base
   def check_in
     if self.check_in_current == true  # User already checked in thru SMS before deadline
       good_check_in_tally
+      schedule_check_in_deadline  # After 24 hours, restart timer
     else 
       send_check_in_sms
+      schedule_check_in_deadline 
     end
   end
 
   # UNTESTED BY RSPEC
   # Schedule multiple delayed job based on number of days and task
-  def schedule_check_in_deadlines
-    number_of = 1
-    self.days_to_complete.downto(1) do |n|  # Value from column
-      job = self.delay(run_at: number_of.days.from_now).check_in # RUN THIS JOB AFTER SCHEDULED TIME
+  def schedule_check_in_deadline
+    if self.days_remaining > 0
+      job = self.delay(run_at: 24.hours.from_now).check_in # RUN THIS JOB AFTER SCHEDULED TIME
       update_column(:delayed_job_id, job.id)  # Update Delayed_job
 
       Delayed::Job.find_by(:id => job.id).update_columns(owner_type: "Micropost")  # Associates delayed_job with Micropost ID
       Delayed::Job.find_by(:id => job.id).update_columns(owner_id: self.id)
-      number_of += 1
+      send_user_status_sms
     end
+  end
+
+  # Provides mapping of goals with active deadlines
+  def send_user_status_sms
     user = User.find_by(:id => self.user_id)
     user.send_status_sms
   end
