e237d844478cf3fb32ee5956f8268a1f8113ac9d
SzeChan <chansd5@gmail.com>
2015-08-03 18:44:12 -0400

    I WILL SURVIVE the HEROKU DEPLOYMENT

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,5 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider    = 'fog/aws'
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
