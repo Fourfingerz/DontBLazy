b41dd70c4d2b3f8d65524745f26fcd23385fdf9b
SzeChan <chansd5@gmail.com>
2015-08-03 18:12:58 -0400

    Reworded AWS access variable, so it hopefully now can push properly to production

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['AWS_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  ENV['AWS_BUCKET']
   end
 end
