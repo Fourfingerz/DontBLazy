98c674c6e539fa0a9457afba06e0476e846b51d4
SzeChan <chansd5@gmail.com>
2015-08-03 20:51:43 -0400

    Removed stuck migration with micropost_id removal from delayed jobs for heroku

--- db/migrate/20150723230801_remove_micropost_id_from_delayed_jobs.rb
+++
@@ -1,6 +0,0 @@
-class RemoveMicropostIdFromDelayedJobs < ActiveRecord::Migration
-  def change
-    remove_column :delayed_jobs, :micropost_type, :string
-    remove_column :delayed_jobs, :micropost_id, :integer
-  end
-end
