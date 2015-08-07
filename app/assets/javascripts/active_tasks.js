a7f9c28450ffb6b105b07e6fd327dc8b949d04a2
SzeChan <chansd5@gmail.com>
2015-08-07 16:35:14 -0400

    Ghetto non functional seconds counter in active_tasks.js. Temp code

--- app/assets/javascripts/active_tasks.js
+++ app/assets/javascripts/active_tasks.js
@@ -9,9 +9,13 @@ for (var i=0, len=rows.length; i<len; i++) {
         var columns = row.getElementsByTagName('td');
         var time = columns[0]
 
-        if (parseInt(time.innerHTML, 10) == 30) {
+        if (parseInt(time.innerHTML, 10) < 1800) {
             time.style.backgroundColor = 'red';
-        } else if (parseInt(time.innerHTML, 10) == 120) {
+        } else if (parseInt(time.innerHTML, 10) > 1800 && < 7200) {
+            time.style.backgroundColor = 'orange';
+        } else if (parseInt(time.innerHTML, 10) > 7200 && < 21600) {
+            time.style.backgroundColor = 'yellow';
+        } else {
             time.style.backgroundColor = 'green';
         }
            
