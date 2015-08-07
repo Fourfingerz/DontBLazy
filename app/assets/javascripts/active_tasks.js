eaa4cb95337edc54ea1e3f3cb81d67b2b72a0303
SzeChan <chansd5@gmail.com>
2015-08-07 15:35:58 -0400

    Wrote JavaScript for active goals list on home page, needs more work in JS to sync with DB

--- app/assets/javascripts/active_tasks.js
+++ app/assets/javascripts/active_tasks.js
@@ -1,22 +1,19 @@
 var table = document.getElementById('active_tasks');
 var tbody = table.getElementsByTagName('tbody')[0];
-var rows  = tbody.getElementsByTagName('tr');
+var rows = tbody.getElementsByTagName('tr');
 
 
 // For each row (active goal), do this:
-for (var i=0, len=rows.length; i<len; i++){
+for (var i=0, len=rows.length; i<len; i++) {
+        var row = rows[i];
+        var columns = row.getElementsByTagName('td');
+        var time = columns[0]
 
- 	// WHAT AM I DOING WITH EACH ROW
-
- 	for each (var row in rows) {
- 		columns = row.getElementsByTagName('td');
-		time = columns[0]
-
-	    if (parseInt(time.innerHTML,10) < 30){
-	        rows[i].style.backgroundColor = 'red';
-	    }
-	    else if (parseInt(time.innerHTML,10) > 120){
-	        rows[i].style.backgroundColor = 'green';
-	    }
-	}
-}
+        if (parseInt(time.innerHTML, 10) == 30) {
+            time.style.backgroundColor = 'red';
+        } else if (parseInt(time.innerHTML, 10) == 120) {
+            time.style.backgroundColor = 'green';
+        }
+           
+};
+            
