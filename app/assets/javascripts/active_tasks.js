97280cd686fccf6b8ddd8121d25abd0d029c0091
SzeChan <chansd5@gmail.com>
2015-08-12 17:14:49 -0400

    Further clarification of variables in active_tasks.js and moved embedded clock data over to title. Adding variable time on dynamic clock depending on checked_in status

--- app/assets/javascripts/active_tasks.js
+++ app/assets/javascripts/active_tasks.js
@@ -9,18 +9,19 @@ window.onload = function() {
         var row = rows[i];
         var columns = row.getElementsByTagName('td');
 
-        var time = columns[0]
-        var seconds_due = time.getAttribute('data-due');
+        var clock_that_shift_colors = columns[0]
+        var embedded_time_in_title = columns[1]
+        var seconds_due = embedded_time_in_title.getAttribute('data-due');
         var seconds = parseInt(seconds_due, 10);
 
         if (seconds < 1800) {                            // Less than 30 minutes
-            time.style.backgroundColor = 'red';
+            clock_that_shift_colors.style.backgroundColor = 'red';
         } else if (seconds > 1800 && seconds < 7200) {   // More than 30 and less than 2 hours
-            time.style.backgroundColor = 'orange';   
+            clock_that_shift_colors.style.backgroundColor = 'orange';   
         } else if (seconds > 7200 && seconds < 21600) {  // More than 2 hours and less than 6 hours 
-            time.style.backgroundColor = 'yellow';
+            clock_that_shift_colors.style.backgroundColor = 'yellow';
         } else {
-            time.style.backgroundColor = 'green';
+            clock_that_shift_colors.style.backgroundColor = 'green';
         }         
     }
 };
