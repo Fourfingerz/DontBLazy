5d15ad7f2870776ac17b91d113825ca20684882c
SzeChan <chansd5@gmail.com>
2015-08-14 19:18:42 -0400

    Added switch to turn off day_already_completed in micropost good_check_in_tally

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -47,6 +47,7 @@ class Micropost < ActiveRecord::Base
   def good_check_in_tally
     if self.days_remaining > 0
       self.days_completed += 1 if self.day_already_completed == false  # DB Column
+      self.day_already_completed = false if self.day_already_completed == true # DB Column
       self.days_remaining -= 1  # DB Column
       self.current_day += 1     # DB Column
       self.check_in_current = false  # Sets this column for next day
