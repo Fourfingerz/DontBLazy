7fbbc31d3445daaf932e7bef6d87239b27eff191
Fourfingerz <chansd5@gmail.com>
2015-08-28 18:48:40 -0400

    Added phony_rails validations for User model to normalize phone_numbers

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,8 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   # DBL
   has_many :recipients, dependent: :destroy
+  phony_normalize :phone_number, :default_country_code => 'US'
+  validates_plausible_phone :phone_number
   serialize :current_tasks_map
   serialize :microposts_due_queue
 
