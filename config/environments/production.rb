4311cf8acb7288449f013bdbb87bcec021b730e1
Fourfingerz <chansd5@gmail.com>
2015-09-06 15:13:20 -0400

    Reversed domain and host in production.rb to correct settings, hopefully...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'www.dontbelazy.today'
+  host = 'dontblazy.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -68,7 +68,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'dontbelazy.today',
     :enable_starttls_auto => true
   }
   
