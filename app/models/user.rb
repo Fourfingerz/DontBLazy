5a89538371778cb2ab92d72bbfefb51db9f53436
Fourfingerz <chansd5@gmail.com>
2015-09-11 15:27:08 -0400

    Fixed user phone uniqueness validation to accept blank at first

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates_uniqueness_of :phone_number
+  validates :phone_number, :uniqueness => true, :allow_blank => true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   # DBL
