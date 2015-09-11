43b6b3be64d9137f94591ddc2b953ecde88ed42a
Fourfingerz <chansd5@gmail.com>
2015-09-11 14:41:59 -0400

    Validates unique phone added to User.rb to PREVENT multiple accounts for a number

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates_uniqueness_of :phone_number
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   # DBL
