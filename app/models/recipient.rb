588a2fdeb49e2d95f6a057346ee63bfb82dbae22
Fourfingerz <chansd5@gmail.com>
2015-09-11 01:20:49 -0400

    Removed validation for uniqueness for accountability buddy phones

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -3,8 +3,7 @@ class Recipient < ActiveRecord::Base
   has_many :micropost_recipients, dependent: :destroy
   has_many :microposts, through: :micropost_recipients
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :phone, presence: true, length: { minimum: 8, maximum: 25 },
-                    uniqueness: { case_sensitive: false }
+  validates :phone, presence: true, length: { minimum: 8, maximum: 25 }
   validates :user_id, presence: true
   accepts_nested_attributes_for :micropost_recipients
 end
