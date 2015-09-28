211b1edfdbef6354eec9c5a931db36ea1f68a0bf
Fourfingerz <chansd5@gmail.com>
2015-09-28 19:21:50 -0400

    Fortified User unit tests in spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe User do
-  # These are DBL phone tests. Rails test suite covers framework functionality.
-  it "has a valid factory" do
+
+  it "should have a valid factory" do
   	expect(build(:user)).to be_valid
   end
 
@@ -11,7 +11,7 @@ describe User do
     expect(user).to be_valid
   end
 
-  it "users cannot make another account with the same phone number" do
+  it "will not allow users to make another account with the same phone number" do
     user = create(:user)
     create(:user,
       id: 1,
@@ -25,4 +25,31 @@ describe User do
     expect(another_phone.errors[:phone_number]).to include('has already been taken')
   end
 
+  it "will not allow users to create account with numbers in either name field" do
+    expect(build(:user,
+                  name: '3CPO',
+                  last_name: 'de Emp1re'
+                  ).should_not be_valid
+                )
+  end
+
+  it "will not allow users to create account with a name that's over 50 characters" do
+    expect(build(:user,
+                  name: 'YZCxeUbMhiNgapHRTaszMQxrcj',
+                  last_name: 'pSsviClrPqwyMeEgpMxfPQiGkw'
+                  ).should_not be_valid 
+              )
+  end
+
+  it "will not allow users to make another account with the same email"
+    first_user = create(:user,
+                    email: 'user@dontbelazy.com'
+                    )
+    second_user = build(:user,
+                  email: 'user@dontbelazy.com'
+                )
+    second_user.valid?
+    expect(second_user.errors[:email]).to include('has already been taken')
+  end
+  
 end
